ebbfc9d0d5a1485f747d8d830c00582e62f0d352
97Fakhreddine <fakhri19971998@gmail.com>
2020-12-17 11:00:49 +0100

    slides show fixed

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-var slideIndex = 0;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+var slideIndex = 1;
 
 function showSlides(n) {
-  var i;
   var slides = document.getElementsByClassName("mySlides");
+  console.log("slides ===>", slides);
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
@@ -19,17 +10,28 @@ function showSlides(n) {
   if (n < 1) {
     slideIndex = slides.length;
   }
-  for (i = 0; i < slides.length; i++) {
+  for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  for (var i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+  console.log("slides ===>", slides.length);
+  console.log("index", slideIndex - 1);
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 // <!-- -----------------------------------------------------slide show in the index page----------------------------------------------------- -->
 function go() {
+  $(".mail-adress").val();
   location.replace("store.html");
 }
 function goback() {
